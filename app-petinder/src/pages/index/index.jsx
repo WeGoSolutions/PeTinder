668e5ed28704b8384d1a2ce1a256c30faf9542a5
Laise1209 <laise.almeida@sptech.school>
2025-04-04 23:30:54 -0300

    Tela do index codada

--- app-petinder/src/pages/index/index.jsx
+++ app-petinder/src/pages/index/index.jsx
@@ -1,10 +1,28 @@
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
+import TextCard from "../../components/TextCard";
+import styles from './index.module.css';
 
-function index() {
+function Index() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.indexContainer}>
+        <NavBar />
+        <div className={styles.firstSection}>
+          <div className={styles.titles}>
+            <span className={styles.title}>PeTinder</span>
+            <span className={styles.subtitle}>Seu melhor amigo a um "swipe" de distância</span>
+          </div>
+          <img className={styles.firstImage} src="./assets/Index/cao-index.png" alt="" />
+          <img className={styles.pawPath} src="./assets/Index/patinhas.png" alt="" />
+        </div>
+        <div className={styles.secondSection}>
+          <img className={styles.secondImage} src="./assets/Index/gato-index.png" alt="" />
+          <TextCard />
+          <img className={styles.paws} src="./assets/Index/patas.png" alt="" />
+        </div>
+        <Footer />
     </div>
   );
 }
 
-export default index;
+export default Index;
