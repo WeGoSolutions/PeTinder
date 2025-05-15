83bdc4676d780f66b39f9495bab08ff53cefbc72
igorchuu <igor.chu@sptech.school>
2025-05-15 18:48:44 -0300

    arrumei o fetch de pet

--- app-petinder/src/pages/initial/initial.jsx
+++ app-petinder/src/pages/initial/initial.jsx
@@ -70,6 +70,56 @@ function Initial() {
         }
     };
 
+
+    const [petIndex, setPetIndex] = useState(0);
+    const [pet, setPet] = useState({
+        tags: [],
+        images: [],
+    });
+
+    const [pets, setPets] = useState([]);
+
+    const aumentarIndex = () => {
+        setPetIndex((prevIndex) => (prevIndex + 1) % pets.length);
+    };
+
+    useEffect(() => {
+        fetch("http://localhost:8080/pets")
+            .then(response => response.json())
+            .then(json => setPets(json))
+            .catch(error => console.error("Error fetching pets:", error));
+    }, []);
+
+    useEffect(() => {
+        if (pets.length > 0 && petIndex < pets.length) {
+            const currentPet = pets[petIndex];
+            const quantTags = currentPet.tags.length;
+            setPet({
+                id: currentPet.id,
+                nome: currentPet.nome,
+                idade: currentPet.idade,
+                curtidas: currentPet.curtidas,
+                isLiked: currentPet.isLiked,
+                descricao: currentPet.descricao,
+                tags: currentPet.tags,
+                qntdTags: quantTags,
+            });
+        }
+    }, [pets, petIndex]);
+
+    useEffect(() => {
+        if (pet.id) {
+            fetch(`http://localhost:8080/pets/${pet.id}/imagens`)
+                .then(response => response.json())
+                .then(json => setPet(prevPet => ({
+                    ...prevPet, // Mantém as propriedades existentes do estado `pet`
+                    images: json // Atualiza apenas a propriedade `images`
+
+                })))
+                .catch(error => console.error("Error fetching pet images:", error));
+        }
+    }, [pet.id]);
+
     const handleCloseModal = async () => {
         const userId = sessionStorage.getItem("userId");
         const authToken = sessionStorage.getItem("authToken");
@@ -135,22 +185,20 @@ function Initial() {
             <NavBar />
             <div className="appArea">
                 <PetActions
-                    images={[]}
-                    adotar={() => { }}
-                    passar={() => { }}
-                />
+                    images={pet.images}
+                    adotar={aumentarIndex}
+                    passar={aumentarIndex} />
                 <PetInfo
-                    petId={0}
-                    petName=""
-                    likes={0}
-                    petAge={0}
-                    petDesc=""
-                    ongLink=""
-                    ongName=""
-                    qntdTags={0}
-                    tags={[]}
-                    isLiked={false}
-                />
+                    petId={pet.id}
+                    petName={pet.nome}
+                    likes={pet.curtidas}
+                    petAge={pet.idade}
+                    petDesc={pet.descricao}
+                    ongLink="https://www.instagram.com/projetoaumigosdobem/"
+                    ongName="AUmigos Do Bem"
+                    qntdTags={pet.qntdTags}
+                    tags={pet.tags}
+                    isLiked={pet.isLiked} />
             </div>
 
             {/* Modal para novos usuários */}
