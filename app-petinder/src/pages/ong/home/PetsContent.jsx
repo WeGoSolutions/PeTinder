68074568d1dc921b91a8cd68db866f43828222f2
Laise1209 <laise.almeida@sptech.school>
2025-05-24 23:25:01 -0300

    Componente do pet

--- app-petinder/src/pages/ong/home/PetsContent.jsx
+++ app-petinder/src/pages/ong/home/PetsContent.jsx
@@ -1,7 +1,15 @@
+import PetCard from "../../../components/ong/PetCard";
+
 export default function PetsContent() {
     return (
-        <div>
-            <h1>PETS</h1>
+        <div className="petContainer">
+            <div className="petHeader">
+                <input type="text" className="searchBar"/>
+                <button className="addPet">Adicionar +</button>
+            </div>
+            <div className="pets">
+                <PetCard size={180} nomePet="Francisco"/>
+            </div>
         </div>
     );
 }
